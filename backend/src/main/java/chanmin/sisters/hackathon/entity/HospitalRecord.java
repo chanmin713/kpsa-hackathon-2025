42708fdb35df81bf55646b72c9ba7f4f6ead9830
seon7129 <seon7129@naver.com>
2025-07-27 00:23:57 +0900

    feat: hospital record 기능 추가

--- backend/src/main/java/chanmin/sisters/hackathon/entity/HospitalRecord.java
+++ backend/src/main/java/chanmin/sisters/hackathon/entity/HospitalRecord.java
@@ -1,5 +1,27 @@
 package chanmin.sisters.hackathon.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Setter
+@Getter
+@Table(name = "hospitalrecord")
 public class HospitalRecord {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long hr_id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    private LocalDateTime datetime;
+    private String location;
+    private String symptom;
+    private String memo;
 }
