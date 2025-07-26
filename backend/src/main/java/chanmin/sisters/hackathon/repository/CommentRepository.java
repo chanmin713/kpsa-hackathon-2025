ae6c8a852f36077a21f7486b0b0e86cea0f13db1
seon7129 <seon7129@naver.com>
2025-07-26 21:11:39 +0900

    feat: user service와 controller 추가

--- backend/src/main/java/chanmin/sisters/hackathon/repository/CommentRepository.java
+++ backend/src/main/java/chanmin/sisters/hackathon/repository/CommentRepository.java
@@ -1,8 +1,9 @@
 package chanmin.sisters.hackathon.repository;
 
+import chanmin.sisters.hackathon.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CommentRepository extends JpaRepository<CommentRepository, Long> {
+public interface CommentRepository extends JpaRepository<Comment, Long> {
 }
